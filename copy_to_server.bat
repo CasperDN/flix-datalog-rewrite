99fa8d424d5e75f61691168e4d8adb873464a31b
Adam Yasser Tallouzi <202004765@post.au.dk>
2025-02-25 10:29:08 +0100

    Fixed file reading bug

--- copy_to_server.bat
+++ copy_to_server.bat
@@ -1,9 +1,13 @@
 @echo off
-set "files=dep\seaborn-data\titanic.csv src\Test\edge.csv"
-set "jar=flix-datalog-rewrite.jar"
-set "program=Path"
+setlocal EnableDelayedExpansion
+set lf= data/
+for /F "delims=" %%i in ('cmd /r dir data /b') do if ("!files!"=="") (set files=%%i) else (set files=!files!%lf%%%i)
 
+set "files=dep\seaborn-data\titanic.csv %files%"
+set "jar=flix-datalog-rewrite.jar"
+echo Building jar...
 java -jar flix.jar build --entrypoint benchmark
 java -jar flix.jar build-jar --entrypoint benchmark
 
+echo Sending to server...
 scp artifact/%jar% %files% flixserver:
--- run_benchmark.bat
+++ run_benchmark.bat
@@ -1,4 +1,5 @@
 @echo off
 set "jar=flix-datalog-rewrite.jar"
 set "program=Path"
+echo Running benchmark...
 ssh flixserver "java -jar %jar% %program%"
